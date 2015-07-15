8266225f4c33e0ae189a8b6703d4cd23732b576d
Jasmine Noack <jasminenoack@gmail.com>
2015-07-14 21:01:13 -0400

    add header test

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,17 @@
+from selenium.webdriver.common.by import By
+
+
+PAGE_HEADER_LOCATOR = (By.ID, "page_header")
+
+
 class BasePage(object):
     def __init__(self, driver):
         self.driver = driver
 
+    @property
     def driver_title(self):
         return self.driver.title
+
+    @property
+    def page_header(self):
+        return self.driver.find_element(*PAGE_HEADER_LOCATOR).text
--- tests/home_page_tests.py
+++ tests/home_page_tests.py
@@ -8,12 +8,18 @@ from pages.home_page import HomePage
 class HomePageTests(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(15)
         self.driver.get("https://www.jasminenoack.com/flallo")
 
     def test_home_page_title(self):
         home = HomePage(self.driver)
-        title = home.driver_title()
-        assert title == "Flallo", "Title is not as expected: {}".format(title)
+        title = home.driver_title
+        assert title == "Flallo"
+
+    def test_home_page_header(self):
+        home = HomePage(self.driver)
+        header = home.page_header
+        assert header == "Welcome to Flallo"
 
     def tearDown(self):
         self.driver.close()
